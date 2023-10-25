d3c490dcb431119810caa8305fa9ab951fa7d2a6
yakshi9 <140039815+yakshi9@users.noreply.github.com>
2023-10-25 15:17:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieComponent from './components/movieComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieComponent />
+    </div>
+  );
+}
+
+export default App;
